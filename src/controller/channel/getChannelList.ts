a915f22b70105886ff91e0afd8c8e5df59c1b9a3
rycont <reactdev@kakao.com>
2020-08-22 19:13:44 +0900

    refactor : linting

--- src/controller/channel/getChannelList.ts
+++ src/controller/channel/getChannelList.ts
@@ -1,37 +1,38 @@
-import { loco } from '../../storage'
+import { loco } from "../../storage";
 // import {IChannel} from '../../types/interfaces'
 import { Request, Response } from "express";
-import { Long, ChannelType, ChatChannel } from '@storycraft/node-kakao';
-import getImageFromChannel from '../../functions/getImageFromChannel';
-import toProcessableChat from '../../functions/toProcessableChat';
-import { IChannel } from '../../types/commonType';
-import chatToTimeStamp from '../../functions/chatToTimeStamp';
+import { Long, ChannelType, ChatChannel } from "@storycraft/node-kakao";
+import getImageFromChannel from "../../functions/getImageFromChannel";
+import toProcessableChat from "../../functions/toProcessableChat";
+import { IChannel } from "../../types/commonType";
+import chatToTimeStamp from "../../functions/chatToTimeStamp";
 
 export const getChannelName = (channel: ChatChannel) => {
-  return channel.Name || channel.DisplayUserInfoList[0].Nickname || '이름 없음'
-}
+  return channel.Name || channel.DisplayUserInfoList[0].Nickname || "이름 없음";
+};
 
 export default (req: Request, res: Response) => {
   const channelList = loco.ChannelManager.getChannelIdList()
     .filter(Boolean)
-    .map(channelId => 
-      loco.ChannelManager.get(channelId))
-      .map(channelInfo => ({
-        profileImage: (channelInfo && getImageFromChannel(channelInfo)) || '',
-        name: channelInfo && getChannelName(channelInfo) || '이름없음',
-        lastMessage: channelInfo?.LastChat && chatToTimeStamp(toProcessableChat(channelInfo?.LastChat)),
-        id: channelInfo?.Id.toString()!
-      })
-  )
+    .map((channelId) => loco.ChannelManager.get(channelId))
+    .map((channelInfo) => ({
+      profileImage: (channelInfo && getImageFromChannel(channelInfo)) || "",
+      name: (channelInfo && getChannelName(channelInfo)) || "이름없음",
+      lastMessage:
+        channelInfo?.LastChat &&
+        chatToTimeStamp(toProcessableChat(channelInfo?.LastChat)),
+      id: channelInfo?.Id.toString()!,
+    }));
   // .filter(Boolean).map
   if (!channelList) {
     res.send({
       status: 400,
-      message: 'Cannot get channelList'
-    })
-    throw `Cannot get channelList`
+      message: "Cannot get channelList",
+    });
+    throw `Cannot get channelList`;
     // return
   }
-  res.send(channelList)
+  console.log(channelList);
+  res.send(channelList);
   // return channelList
-}
+};
