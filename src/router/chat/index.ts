8f7a6c71e0add030a3e871f2a93d49b58d5875c6
rycont <reactdev@kakao.com>
2020-08-30 22:14:42 +0900

    fix : remove param unused param from getChat(logId)

--- src/router/chat/index.ts
+++ src/router/chat/index.ts
@@ -4,7 +4,7 @@ import send from "../../controller/chat/send";
 import { multerConfig } from "../../storage";
 
 const router = Router();
-router.get("/:channelId/:logId", getChat);
+router.get("/:channelId/", getChat);
 router.post("/send", multerConfig.array("attachment"), send);
 
 export default router;
