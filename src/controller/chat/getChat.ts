38268e39e85014a572ceac251cee0c4be881804d
RyCont (정한) <35295182+rycont@users.noreply.github.com>
2020-08-30 22:21:57 +0900

    feat : Get all gettable chatlog from server (#3)
    * feat : get all chatdata from locoserver
    
    * refactor : remove console
    
    * fix : remove param unused param from getChat(logId)

--- src/controller/chat/getChat.ts
+++ src/controller/chat/getChat.ts
@@ -1,24 +1,40 @@
 import { Request, Response } from "express";
-import { Long } from "@storycraft/node-kakao";
+import { Long, Chat } from "@storycraft/node-kakao";
 import { loco } from "../../storage";
 import toProcessableChat from "../../functions/toProcessableChat";
-const getChat = async (req: Request, res: Response) => {
-  const { channelId, logId } = req.params;
-  const longgedChannelId = Long.fromString(channelId);
-  // const channel = loco.ChannelManager.get(longgedChannelId);
 
-  const chatLog = await loco.ChatManager.getChatListFrom(
-    longgedChannelId,
-    Number(logId)
-  );
-  if (chatLog.status !== 0) {
+const recursivelyGetChats = async (channelId: Long) => {
+  let fullChats: Chat[] = [];
+  let fullChatIds: string[] = [];
+  let lastChatId = 0;
+  while (true) {
+    const { result } = await loco.ChatManager.getChatListFrom(
+      channelId,
+      lastChatId
+    );
+    if (!result) return fullChats;
+    if (!result.length) return fullChats;
+    const ids: string[] = result.map((e) => e.LogId.toString());
+    fullChatIds = [...fullChatIds, ...ids.slice(1)];
+    fullChats = [...fullChats, ...result.slice(1)];
+    if (result.length !== 90) return fullChats;
+    lastChatId = +result.slice(-1)[0].LogId.toString();
+  }
+};
+
+const getChat = async (req: Request, res: Response) => {
+  const { channelId } = req.params;
+  const longgedChannelId = Long.fromString(channelId);
+
+  const chatLog = await recursivelyGetChats(longgedChannelId);
+  if (!chatLog.length) {
     res.status(500).send({
       status: 500,
       message: "Cannot log message",
     });
     return;
   }
-  const proceed = chatLog.result?.map(toProcessableChat);
+  const proceed = chatLog?.map(toProcessableChat);
   loco.ChannelManager.get(longgedChannelId)?.markChannelRead(
     Long.fromString("" + proceed?.slice(-1)[0].chatId)
   );
